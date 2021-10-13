0d75bf4fddf0586eba5fe96e4a3a85b7afbfaa2b
tsw0603 <209410785@gms.tku.edu.tw>
2021-10-13 20:21:03 +0800

    URL

--- utils/database.js
+++ utils/database.js
@@ -1,11 +1,14 @@
 const { Pool } = require('pg');
 
+const isProduction = process.NODE_ENV === "production"
+
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'crown_85',
-    password: '0000',
-    port: '5432',
+    //user: 'postgres',
+    //host: 'localhost',
+    //database: 'crown_85',
+    //password: '0000',
+    //port: '5432',
+    connectionString: isProduction ? process.env.DATABASE_URL : `postgresql://postgres:0000@localhost:5432/crown_85`
 });
 
 pool.query('SELECT * from category_85', (err, res) => {
